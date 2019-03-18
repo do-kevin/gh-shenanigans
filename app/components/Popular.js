18eef18f33e96c20a55c5698555607e94555a33c
Kevin Do <do.kevin.vo@gmail.com>
2019-03-17 18:42:05 -0700

    Refactors code for api call

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,8 +1,9 @@
 var React = require('react');
 var PropTypes = require('prop-types');
+var api = require('../utils/api');
 
 function SelectLanguage(props) {
-  console.log(props);
+	console.log(props);
 	var languages = [ 'All', 'JavaScript', 'Ruby', 'Java', 'Python' ];
 
 	return (
@@ -34,28 +35,92 @@ SelectLanguage.propTypes = {
 	onSelect: PropTypes.func.isRequired
 };
 
+function RepoGrid(props) {
+	return (
+		<ul className="popular-list">
+			{props.repos.map(function(repo, index) {
+				return (
+					<li className="popular-item" key={repo.name}>
+						<div className="popular-rank">#{index + 1}</div>
+						<ul className="space-list-items">
+							<li>
+								<img
+									src={repo.owner.avatar_url}
+									alt={'Avatar for ' + repo.owner.login}
+									className="avatar"
+								/>
+							</li>
+							<li>
+								<a className="repo-name"href={repo.html_url}>{repo.name}</a>
+							</li>
+							<li>@{repo.owner.login}</li>
+							<li>
+								<svg
+									className="octicon-star"
+									viewBox="0 0 14 16"
+									version="1.1"
+									width="14"
+									height="16"
+								>
+									<path
+										fillRule="evenodd"
+										d="M14 6l-4.9-.64L7 1 4.9 5.36 0 6l3.6 3.26L2.67 14 7 11.67 11.33 14l-.93-4.74L14 6z"
+									/>
+								</svg>
+                {' '}
+								{repo.stargazers_count}
+							</li>
+						</ul>
+					</li>
+				);
+			})}
+		</ul>
+	);
+}
+
+RepoGrid.propTypes = {
+	repos: PropTypes.array.isRequired
+};
+
 class Popular extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			languageSelected: 'All'
+			languageSelected: 'All',
+			repos: null
 		};
 
 		this.updateLanguage = this.updateLanguage.bind(this);
 	}
 
+	componentDidMount() {
+		this.updateLanguage(this.state.languageSelected);
+	}
+
 	updateLanguage(lang) {
 		this.setState(function() {
 			return {
-				languageSelected: lang
+				languageSelected: lang,
+				repos: null
 			};
 		});
+
+		api.fetchPopularRepos(this.state.languageSelected).then(
+			function(repos) {
+				this.setState(function() {
+					return {
+						repos: repos
+					};
+				});
+			}.bind(this)
+		);
 	}
 
 	render() {
 		return (
 			<div>
 				<SelectLanguage languageSelected={this.state.languageSelected} onSelect={this.updateLanguage} />
+        {!this.state.repos ? (<div className="loader">Loading...</div>) : <RepoGrid repos={this.state.repos} />}
 			</div>
 		);
 	}
