c2f969f5a78ff90d9a1491dcac2028a80fb6fa47
Kevin Do <do.kevin.vo@gmail.com>
2019-03-17 19:55:30 -0700

    Implements react-router-dom

--- app/components/App.js
+++ app/components/App.js
@@ -1,14 +1,33 @@
 var React = require('react');
 var Popular = require('./Popular');
+var ReactRouter = require('react-router-dom');
+var Router = ReactRouter.BrowserRouter;
+var Route = ReactRouter.Route;
+var Switch = ReactRouter.Switch;
+var Nav = require('./Nav');
+var Home = require('./Home');
+var Battle = require('./Battle');
 
 class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <Popular/>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<Router>
+				<div>
+					<Nav />
+					<Switch>
+						<Route exact path="/" component={Home} />
+						<Route path="/popular" component={Popular} />
+						<Route exact path="/battle" component={Battle} />
+						<Route
+							render={function() {
+								return (<p style={{color: 'hsl(355, 70%, 46%)', textAlign: 'center', fontSize: '48px', fontWeight: 600}}>Not Found</p>);
+							}}
+						/>
+					</Switch>
+				</div>
+			</Router>
+		);
+	}
 }
 
 module.exports = App;
